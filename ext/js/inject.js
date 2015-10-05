4df8448d7472e01634e8dbb4dadb5aefa4dd7605
Eshin Kunishima <omochi@luna.miko.im>
2015-10-06 08:34:33 +0900

    Format inject.js

--- ext/js/inject.js
+++ ext/js/inject.js
@@ -1,50 +1,49 @@
-chrome.extension.sendMessage({}, function (response) {
-    var readyStateCheckInterval = setInterval(function () {
-        if (document.readyState === "complete") {
-            clearInterval(readyStateCheckInterval);
+chrome.extension.sendMessage({}, function(response) {
+  var readyStateCheckInterval = setInterval(function() {
+    if (document.readyState === "complete") {
+      clearInterval(readyStateCheckInterval);
 
-            var url = null;
+      var url = null;
 
-            switch ($(location).attr('hostname')) {
-                case "photohito.com":
-                    url = $("meta[property='og:image']").attr("content");
-                    url = url.replace("_s", "_o");
-                    url = url.replace("_m", "_o");
-                    break;
+      switch ($(location).attr('hostname')) {
+        case "photohito.com":
+          url = $("meta[property='og:image']").attr("content");
+          url = url.replace("_s", "_o");
+          url = url.replace("_m", "_o");
+          break;
 
-                case "twitter.com":
-                    url = $("img[alt = '埋め込み画像への固定リンク']").attr("src");
-                    if (url != null) {
-                        url += ":orig";
-                    }
-                    break;
+        case "twitter.com":
+          url = $("img[alt = '埋め込み画像への固定リンク']").attr("src");
+          if (url != null) {
+            url += ":orig";
+          }
+          break;
 
-                case "ganref.jp":
-                    url = $("meta[property='og:image']").attr("content");
-                    url = url.replace(/thumb[0-9]/, "original");
-                    break;
+        case "ganref.jp":
+          url = $("meta[property='og:image']").attr("content");
+          url = url.replace(/thumb[0-9]/, "original");
+          break;
 
-                case "photozou.jp":
-                    url = $("img[itemprop = 'image']").attr("src");
-                    url = url.replace(/_[0-9]+\./, "_org.");
-                    break;
+        case "photozou.jp":
+          url = $("img[itemprop = 'image']").attr("src");
+          url = url.replace(/_[0-9]+\./, "_org.");
+          break;
 
-                case "acafe.msc.sony.jp":
-                    url = $("meta[property='og:image']").attr("content");
-                    url = decodeURIComponent(url);
-                    url = url.match(/url=(.+)&mode/)[1];
-                    break;
+        case "acafe.msc.sony.jp":
+          url = $("meta[property='og:image']").attr("content");
+          url = decodeURIComponent(url);
+          url = url.match(/url=(.+)&mode/)[1];
+          break;
 
-                case "www.flickr.com":
-                    break;
-            }
+        case "www.flickr.com":
+          break;
+      }
 
-            if (url != null) {
-                //window.open(url, '_blank');
-                window.prompt('', url);
-                //window.open(url,'Download');
-            }
-            ;
-        }
-    }, 3);
+      if (url != null) {
+        //window.open(url, '_blank');
+        window.prompt('', url);
+        //window.open(url,'Download');
+      };
+    }
+  }, 3);
 });
