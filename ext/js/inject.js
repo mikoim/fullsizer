1f9342381c7093a472d7d3b31352923601ad672b
Eshin Kunishima <ek@esh.ink>
2016-12-02 02:23:16 +0900

    change jQuery to native API

--- ext/js/inject.js
+++ ext/js/inject.js
@@ -5,32 +5,32 @@ chrome.extension.sendMessage({}, function(response) {
 
       var url = null;
 
-      switch ($(location).attr('hostname')) {
+      switch (window.location.hostname) {
         case "photohito.com":
-          url = $("meta[property='og:image']").attr("content");
+          url = document.querySelector("meta[property='og:image']").getAttribute("content");
           url = url.replace("_s", "_o");
           url = url.replace("_m", "_o");
           break;
 
         case "twitter.com":
-          url = $("img[alt = '埋め込み画像への固定リンク']").attr("src");
+          url = document.querySelector("img[alt = '埋め込み画像への固定リンク']").getAttribute("src");
           if (url != null) {
             url += ":orig";
           }
           break;
 
         case "ganref.jp":
-          url = $("meta[property='og:image']").attr("content");
+          url = document.querySelector("meta[property='og:image']").getAttribute("content");
           url = url.replace(/thumb[0-9]/, "original");
           break;
 
         case "photozou.jp":
-          url = $("img[itemprop = 'image']").attr("src");
+          url = document.querySelector("img[itemprop = 'image']").getAttribute("src");
           url = url.replace(/_[0-9]+\./, "_org.");
           break;
 
         case "acafe.msc.sony.jp":
-          url = $("meta[property='og:image']").attr("content");
+          url = document.querySelector("meta[property='og:image']").getAttribute("content");
           url = decodeURIComponent(url);
           url = url.match(/url=(.+)&mode/)[1];
           break;
@@ -41,7 +41,7 @@ chrome.extension.sendMessage({}, function(response) {
 
       if (url != null) {
         chrome.runtime.sendMessage({url: url}, function(response){});
-      };
+      }
     }
   }, 3);
 });
